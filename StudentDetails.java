2f944c5503a424aafa252a7f39cb5046d5a50aa5
mishranshu <mishranshu94@gmail.com>
2025-04-17 10:32:39 +0530

    file added to local respository

---
+++ StudentDetails.java
@@ -0,0 +1,16 @@
+class StudentDetails
+{
+	public static void main(String [] args)
+{
+		System.out.println("Good Morning Student");
+		
+		System.out.println("welcome to the University");
+
+		System.out.println("Hope You are doing Good");
+		
+		System.out.println("Have a Great Day");
+
+
+}
+
+}
